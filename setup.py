5ef53f542cda882b946abe2df1626483b85a47e4
David Jelic <djelic@buksna.net>
2010-12-01 00:44:49 +0100

    Add packaging script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name='pyrosapi',
+  version='0.1',
+  author='David Jelic',
+  author_email='djelic@buksna.net',
+#  packages=['RosAPI',],
+  url='http://git.buksna.net/?p=python-rosapi.git',
+  description='Python binding for Mikrotik RouterOS API',
+  py_modules=['RosAPI'],
+)
